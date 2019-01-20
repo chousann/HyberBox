c92b7dd03ce700bd656f68a4df60031e60007443
chousann <45139336+chousann@users.noreply.github.com>
2019-01-20 20:14:22 +0800

    Add files via upload

---
+++ win32.h
@@ -0,0 +1,13 @@
+
+#if !defined(AFX_WIN32_H__5B67EFE0_BB98_4D16_9B8A_41A404D074B9__INCLUDED_)
+#define AFX_WIN32_H__5B67EFE0_BB98_4D16_9B8A_41A404D074B9__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "resource.h"
+#include "scene.h"
+
+
+#endif // !defined(AFX_WIN32_H__5B67EFE0_BB98_4D16_9B8A_41A404D074B9__INCLUDED_)
